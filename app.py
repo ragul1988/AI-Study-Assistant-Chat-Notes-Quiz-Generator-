3b71c6d8eff3e8de8844a6bec3db9ff8c50ad140
ragul1988 <ragulvenkat999@gmail.com>
2026-05-18 09:40:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ except:
 
 genai.configure(api_key=GEMINI_API_KEY)
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 # =========================
 # PDF INPUT
